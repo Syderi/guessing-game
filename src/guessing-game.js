5c37e274008580a32ca78a67ba8267f91cf3bfc1
Syderi <syderi@yahoo.com>
2022-08-03 10:45:47 +0300

    feat: all right

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round(((this.max - this.min) / 2) + this.min);
+        // console.log('this.result', this.result);
+        return this.result;
     }
 
     lower() {
 
+        this.max = this.result;
+        // return this.max
     }
 
     greater() {
-
+        this.min = this.result;
+        // return this.min;
     }
 }
 
